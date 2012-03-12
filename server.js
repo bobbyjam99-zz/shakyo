28c741014e2441cbe14a2fa13a1da7caa264ec31
Tomokazu IMAI <bobbyjam99@gmail.com>
2012-03-12 11:57:35 +0900

    add request handle

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     console.log("Request received.");
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
