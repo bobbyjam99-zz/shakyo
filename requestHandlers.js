27026b9cc3597fa2e4bbef9dcc4a4fa5a13658d7
Tomokazu IMAI <bobbyjam99@gmail.com>
2012-03-18 20:07:15 +0900

    handle post data.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>' +
@@ -19,10 +19,10 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
 
