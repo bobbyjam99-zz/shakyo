68b5fd34c1bad8f78d7925740add3bf1115601a9
Tomokazu IMAI <bobbyjam99@gmail.com>
2012-03-18 20:11:25 +0900

    add querystring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,5 @@
+var querystring = require("querystring");
+
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
@@ -22,7 +24,8 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent the text: " +
+  querystring.parse(postData).text);
   response.end();
 }
 
