ae7c91b767ee35f7f85f8c7f355e14724e47b6e8
Tomokazu IMAI <bobbyjam99@gmail.com>
2012-03-20 00:18:18 +0900

    change textarea to file

--- requestHandlers.js
+++ requestHandlers.js
@@ -10,9 +10,10 @@ function start(response, postData) {
     'charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" ' +
+    'method="post">' +
+    '<input type="file" name="upload">' + 
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
